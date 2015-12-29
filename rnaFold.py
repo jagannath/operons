f2c628403cbe1d50757bbf076062b4f0534881f7
jagannath <jagannath@utexas.edu>
2015-12-29 17:16:12 -0600

    Updated the Ecoli operonic information (the heatmap; phylogenetic distance) in the notebook. I am using regulonDB as the source of E.coli operon information.

---
+++ rnaFold.py
@@ -0,0 +1,16 @@
+#! /home/anaconda/bin/python2.7
+import os
+import sys
+
+sys.path.append('/home/jaggu/research/downloads/vienna/viennaRNA/lib/python2.7/site-packages')
+import RNA
+
+out = RNA.fold('CCCCCAAAGGGGG')
+print out
+
+
+
+
+
+
+
